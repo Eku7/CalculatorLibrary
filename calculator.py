36f715c7ee95c1398b6e820cc28694213029abde
Ekesh Garg <ekesh.gargeg@gmail.com>
2019-11-01 16:04:00 +0530

    Errors Removed

--- calculator.py
+++ calculator.py
@@ -1,7 +1,9 @@
 "Calculator code"
 
-def add(first_term,second_term):
+
+def add(first_term, second_term):
     return first_term + second_term
 
-def subtract(first_term,second_term):
+
+def subtract(first_term, second_term):
     return first_term - second_term
